46e939aee8a15f71f5390968d0c474ec6f17af17
supriya <supriya.sv@profinch.com>
2020-03-03 14:30:08 +0530

    initial project add

---
+++ .idea/src/test/java/sup/springframework/jokes/spring5jokesapp/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sup.springframework.jokes.spring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
